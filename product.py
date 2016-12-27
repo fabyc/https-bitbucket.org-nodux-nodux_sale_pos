bfca00fb80fe8dfd43a0dc4d3117077fe2eee48d
Tatiana Quizhpe <tatianaq@nodux.ec>
2016-12-27 11:45:17 -0500

    Fixed rec name product

--- product.py
+++ product.py
@@ -70,46 +70,50 @@ class Product():
     def get_rec_name(self, name):
         Shop = Pool().get('sale.shop')
         shop = Shop(Transaction().context.get('shop'))
-        warehouse = shop.no_warehouse
-        Location = Pool().get('stock.location')
-        locations = Location.search([('type','=', 'warehouse')])
-        grouping=('product',)
-        warehouses = ""
-        qty = 0
-        i = 0
-        if warehouse == 0:
-            if self.code:
-                return '[' + self.code + '] ' + self.name
-            else:
-                return self.name
 
-        elif warehouse == None:
-            for l in locations:
-                location_ids = [l.id]
-                cantidad = self.products_by_location(location_ids=location_ids,
-                    product_ids=[self.id], with_childs=True,
-                    grouping=grouping)
-                for clave, valor in cantidad.iteritems():
-                    qty = (str(valor).split("."))[0]
-                warehouses += l.name+ ':'+ str(qty)
+        if shop.id:
+            warehouse = shop.no_warehouse
+            Location = Pool().get('stock.location')
+            locations = Location.search([('type','=', 'warehouse')])
+            grouping=('product',)
+            warehouses = ""
+            qty = 0
+            i = 0
+            if warehouse == 0:
+                if self.code:
+                    return '[' + self.code + '] ' + self.name
+                else:
+                    return self.name
 
-            if self.code:
-                return '[' + self.code + '] ' + self.name + ' (Precio:'+str(self.list_price)+' - '+warehouses+')'
-            else:
-                return self.name
+            elif warehouse == None:
+                for l in locations:
+                    location_ids = [l.id]
+                    cantidad = self.products_by_location(location_ids=location_ids,
+                        product_ids=[self.id], with_childs=True,
+                        grouping=grouping)
+                    for clave, valor in cantidad.iteritems():
+                        qty = (str(valor).split("."))[0]
+                    warehouses += l.name+ ':'+ str(qty)
 
-        elif warehouse > 0 :
-            if i < warehouse:
-                location = locations[i]
-                location_ids = [location.id]
-                cantidad = self.products_by_location(location_ids=location_ids,
-                    product_ids=[self.id], with_childs=True,
-                    grouping=grouping)
-                for clave, valor in cantidad.iteritems():
-                    qty = (str(valor).split("."))[0]
-                warehouses += location.name+ ':'+ str(qty)
-                i += 1
-            if self.code:
-                return '[' + self.code + '] ' + self.name + ' (Precio:'+str(self.list_price)+' - '+warehouses+')'
-            else:
-                return self.name
+                if self.code:
+                    return '[' + self.code + '] ' + self.name + ' (Precio:'+str(self.list_price)+' - '+warehouses+')'
+                else:
+                    return self.name
+
+            elif warehouse > 0 :
+                if i < warehouse:
+                    location = locations[i]
+                    location_ids = [location.id]
+                    cantidad = self.products_by_location(location_ids=location_ids,
+                        product_ids=[self.id], with_childs=True,
+                        grouping=grouping)
+                    for clave, valor in cantidad.iteritems():
+                        qty = (str(valor).split("."))[0]
+                    warehouses += location.name+ ':'+ str(qty)
+                    i += 1
+                if self.code:
+                    return '[' + self.code + '] ' + self.name + ' (Precio:'+str(self.list_price)+' - '+warehouses+')'
+                else:
+                    return self.name
+        else:
+            return self.name
